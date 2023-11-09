a562152aaed8aa0e6399824321cd5969ebd4afcb
Jaime <jaime.corrales@fptxurdinaga.com>
2023-11-09 12:30:37 +0100

    Jaime Commit Scroll

--- app/src/main/java/com/example/golden00/MainActivity.kt
+++ app/src/main/java/com/example/golden00/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.golden00.databinding.ActivityCuestionario01Binding
 import com.example.golden00.databinding.ActivityCuestionario1Binding
 import com.example.golden00.databinding.ActivityMainBinding
 
